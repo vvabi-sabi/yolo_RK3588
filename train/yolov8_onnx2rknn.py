86c49ea9b9bb6e777798f6157b85bf0a265a02be
vvabi-sabi <v.belotserkovskii@g.nsu.ru>
2025-07-14 11:44:01 +0700

    change convertation onnx2rknn script

---
+++ train/yolov8_onnx2rknn.py
@@ -0,0 +1,47 @@
+import sys
+from rknn.api import RKNN
+
+model_name = 'yolov8'
+
+DEFAULT_RKNN_PATH = f'./models/{model_name}.rknn'
+DEFAULT_QUANT = False
+
+
+if __name__ == '__main__':
+    model_path, platform, do_quant, output_path = f"../models/{model_name}.onnx", "rk3588", False, DEFAULT_RKNN_PATH
+
+    # Create RKNN object
+    rknn = RKNN(verbose=True)
+
+    # Pre-process config
+    print('--> Config model')
+    rknn.config(mean_values=[[0, 0, 0]], std_values=[
+                    [255, 255, 255]], target_platform=platform)
+    print('done')
+
+    # Load model
+    print('--> Loading model')
+    ret = rknn.load_onnx(model=model_path)
+    if ret != 0:
+        print('Load model failed!')
+        exit(ret)
+    print('done')
+
+    # Build model
+    print('--> Building model')
+    ret = rknn.build(do_quantization=do_quant) #, dataset=DATASET_PATH)
+    if ret != 0:
+        print('Build model failed!')
+        exit(ret)
+    print('done')
+
+    # Export rknn model
+    print('--> Export rknn model')
+    ret = rknn.export_rknn(output_path)
+    if ret != 0:
+        print('Export rknn model failed!')
+        exit(ret)
+    print('done')
+
+    # Release
+    rknn.release()
